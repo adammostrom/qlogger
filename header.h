dbb10ffb3ed4512e30d68e7cb3b56485c4f841c1
Adam Mos <moztrom93@gmail.com>
2025-09-26 00:32:37 +0200

    automatic push

---
+++ header.h
@@ -0,0 +1,9 @@
+#include <ncurses.h>
+#include <ctime>
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <vector>
+
+//string get_date();
--- tutorial.cpp
+++ tutorial.cpp
@@ -1,6 +1,16 @@
-#include <ncurses.h>
+#include "header.h"
+
+
+using namespace std;
+using namespace std::chrono;
+
+string get_date();
 
 int main() {
+
+    string date = get_date();
+    cout << date;
+
     initscr();                // Start ncurses mode
     cbreak();                 // Disable line buffering
     noecho();                 // Don't echo typed characters
@@ -13,6 +23,10 @@ int main() {
     int x = 0, y = 1;         // initialize coords
 
     while ((ch = getch()) != 'q') {  // loop until q pressed
+        string date = get_date();
+        int size = date.length();
+        x = size + 2;
+
         printw("%c", ch);
         refresh();
         x++;                  // move forward each time
@@ -21,7 +35,9 @@ int main() {
             y++;
             x = 0; // reset x to start of line
             move(y, x);
-            printw("> ");
+            
+
+            printw("%s > ", date.c_str()); // use c_str() here
         }
     }
 
@@ -30,6 +46,13 @@ int main() {
 }
 
 
-int get_date(){
-    
+
+
+string get_date(){
+    auto now_f = system_clock::now();
+    time_t now_c = system_clock::to_time_t(now_f); // Now first
+
+    ostringstream oss;
+    oss << std::put_time(localtime(&now_c), "%Y-%m-%d - %H:%M:%S");
+    return oss.str();
 }
